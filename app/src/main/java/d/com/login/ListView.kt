262adfc7add0adcbd17bcd50fd64ee5f3d823959
Abax <abassife@gmail.com>
2021-05-09 02:41:23 +0100

    Android login design

---
+++ app/src/main/java/d/com/login/ListView.kt
@@ -0,0 +1,11 @@
+package d.com.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+    }
+}
---
+++ app/src/main/java/d/com/login/MainActivity.kt
@@ -0,0 +1,19 @@
+package d.com.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun logIn(view: View): Unit {
+
+        val intent: Intent = Intent(this,ListView::class.java);
+        startActivity(intent);
+    }
+}
